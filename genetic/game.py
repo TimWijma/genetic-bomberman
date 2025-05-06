0ba079703a6aa8caae7ac349d381b2c5fb40b137
TimWijma <tim.wijma@gmail.com>
2025-05-06 16:23:53 +0200

    allow custom hardcoded maps

--- genetic/game.py
+++ genetic/game.py
@@ -2,16 +2,37 @@ import pommerman
 from pommerman import agents
 from genetic.test_agent import TestAgent
 from typing import List
+import numpy as np
 
 
 class Game:
-    def __init__(self, tournament_name: str, agent_list: List[agents.BaseAgent]):
+    def __init__(
+            self, 
+            agent_list: List[agents.BaseAgent], 
+            tournament_name: str = "PommeFFACompetition-v0",
+            custom_map: List[List[int]] = None,
+        ):
         self.env = pommerman.make(tournament_name, agent_list)
+        self.custom_map = custom_map
+
+        if custom_map is not None:
+            self._set_map(custom_map)
+
+    def _set_map(self, custom_map: List[List[int]]):
+        custom_map = np.array(custom_map, dtype=np.uint8)
+        assert custom_map.shape == (11, 11), "Custom map must be of shape (11, 11)"
+        assert np.all(np.isin(custom_map, [0, 1, 2])), "Custom map must contain only values 0, 1, 2, or 3"
+        
+        self.env._board = custom_map
 
     def play_game(self, num_episodes: int = 1, render_mode: str = None):
         '''Simple function to bootstrap a game.'''
         for i_episode in range(num_episodes):
             state = self.env.reset()
+
+            if self.custom_map is not None:
+                self._set_map(self.custom_map)
+
             done = False
             while not done:
                 if render_mode is not None:
--- genetic/test.py
+++ genetic/test.py
@@ -6,13 +6,30 @@ from genetic.game import Game
 def main():
     '''Simple function to bootstrap a game.'''
 
-    game = Game('PommeFFACompetition-v0', [
+    custom_map = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  # Border walls
+        [1, 0, 0, 0, 2, 0, 2, 0, 0, 0, 1],  # Player 0 starting area
+        [1, 0, 1, 2, 1, 2, 1, 2, 1, 0, 1],
+        [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],
+        [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1],
+        [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],  # Middle row
+        [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1],
+        [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],
+        [1, 0, 1, 2, 1, 2, 1, 2, 1, 0, 1],
+        [1, 0, 0, 0, 2, 0, 2, 0, 0, 0, 1],  # Player 3 starting area
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  # Border walls
+    ]
+
+    game = Game([
         TestAgent(),
         TestAgent(),
-    ])
+    ], 
+        tournament_name="PommeFFACompetition-v0",
+        custom_map=custom_map,
+    )
 
     game.play_game(num_episodes=1)
-    game.play_game(num_episodes=1)
+    # game.play_game(num_episodes=1)
 
 if __name__ == '__main__':
     main()
